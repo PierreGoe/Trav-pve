9283e1dd13967fab5dca05db9c172e27706d8a60
pierre.goemans <pierre.goemans.ext@melting-point.fr>
2022-10-30 15:57:05 +0100

    Catch all route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,41 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   
     {
-      path: "/start",
-      name: "home",
-      component: () => import("../views/HomeView.vue"),
+      path: "/",
+      name: "WorldMap",
+      component: () => import("../views/WorldMapView.vue"),
+      meta: {
+        title: "World Map",
+      },
     },
     {
       path: "/map",
       name: "WorldMap",
       component: () => import("../views/WorldMapView.vue"),
+      meta: {
+        title: "World Map",
+      },
     },
     {
       path: "/valley/:id",
       name: "ValleyMap",
       component: () => import("../views/ValleyView.vue"),
+      meta: {
+        title: "Valley Map",
+      },
+    },
+    {
+      name: "NotFound",
+      path: "/:pathMatch(.*)*",
+      redirect: { name: "WorldMap" },
     },
   ],
 });
 
+router.afterEach((to, from) => {
+  document.title = to.meta.title ? to.meta.title! : "Travi";
+});
 export default router;
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import MenuGame from "../components/MenuGame.vue";
-</script>
-
-<template>
-  <main>
-    <MenuGame />
-  </main>
-</template>
