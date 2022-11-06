4eecced926349bb239a21e2f83275959c3b97151
pierre.goemans <pierre.goemans.ext@melting-point.fr>
2022-11-06 18:13:10 +0100

    fix

--- src/components/Button/ResetButton.vue
+++ src/components/Button/ResetButton.vue
@@ -4,12 +4,16 @@
 <script setup lang="ts">
 import { useRessourcesStore } from "../../stores/ressources";
 import { useWorldMapStore } from "../../stores/worldMap";
+import { useRouter, useRoute } from "vue-router";
 
+const router = useRouter();
+const route = useRoute();
 const ressourcesStore = useRessourcesStore();
 const worldMapStore = useWorldMapStore();
 
 function resetGame() {
   ressourcesStore.resetRessources();
   worldMapStore.resetWorldMap();
+  router.go(route.path);
 }
 </script>
